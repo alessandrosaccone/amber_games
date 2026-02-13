144c015d0eab00916c9a25e559d72853c35d8490
alessandrosaccone <alessandrosaccone96@gmail.com>
2026-02-13 22:36:11 +0100

    bcryptis

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Invece di 'bcrypt'
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
