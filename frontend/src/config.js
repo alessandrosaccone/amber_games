77bb71d024422244fe571155b60ea32cacd0ca40
alessandrosaccone <alessandrosaccone96@gmail.com>
2026-02-14 04:43:35 +0100

    rollback

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // API Configuration
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000' || 'https://triumvitavolo.onrender.com';
+const API_BASE_URL = process.env.REACT_APP_API_URL || '';
 
 export default API_BASE_URL;
